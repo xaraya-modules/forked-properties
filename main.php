240a1292ce0a8d267c4403482f3d5e15bc480ce4
mikespub <github@mikespub.net>
2024-04-02 21:41:03 +0200

    add twig templates

--- main.php
+++ main.php
@@ -69,11 +69,21 @@ class IconCheckboxProperty extends CheckboxProperty
             $result = xarVar::prepForDisplay($result);
         }
         $data['icon_option'] = $result;
+        $data['iconpath'] = null;
+        if (!empty($data['icon_directory']) && !empty($data['icon_option'])) {
+            $iconpath = sys::code() . 'properties/iconcheckbox/xartemplates/icons/' . $data['icon_directory'] . '/' . $data['icon_option'];
+            if (file_exists($iconpath) && !is_dir($iconpath)) {
+                $data['iconpath'] = $iconpath;
+            }
+        }
         return parent::showOutput($data);
     }
 
     public function getIconOptions()
     {
+        if (is_array($this->initialization_icon_options)) {
+            return $this->initialization_icon_options;
+        }
         $options = [];
         $lines = explode(';', $this->initialization_icon_options);
         // remove the last (empty) element
